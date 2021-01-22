672ccf2bf65856f36dbed648452a542b367172c6
tony890724 <76900304+tony890724@users.noreply.github.com>
2021-01-23 03:59:29 +0800

    function

--- function.py
+++ function.py
@@ -3,8 +3,8 @@ def bubble_sort(num):
     i = quantity
 
     while i > 1:
-        for j in range(len(num) - 1):
-            if(num[j] > num[j+1]):
+        for j in range(i - 1):
+            if(num[j] < num[j+1]):
                 num[j] = num[j] + num[j+1]
                 num[j+1] = num[j] - num[j+1]
                 num[j] = num[j] - num[j+1]
@@ -19,5 +19,4 @@ for i in range(quantity):
     temp = int(input())
     num.append(temp)
     
-
 print(bubble_sort(num))
