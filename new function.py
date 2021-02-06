cee56e47728e3986aac675f502a6eb954b225092
tony890724 <76900304+tony890724@users.noreply.github.com>
2021-02-06 17:11:34 +0800

    new function

--- new function.py
+++ new function.py
@@ -1,11 +1,11 @@
 def bubble_sort(num): 
-    i = quantity
+    i = len(num)
     while i > 1: 
         for j in range(i - 1):
             if(num[j] < num[j+1]): #判別前後數字大小 以調整順序
-                num[j] = num[j] + num[j+1]
-                num[j+1] = num[j] - num[j+1]
-                num[j] = num[j] - num[j+1]
+                num[j] = num[j] ^ num[j+1]
+                num[j+1] = num[j] ^ num[j+1]
+                num[j] = num[j] ^ num[j+1]
         i -= 1 #因為已將最小的數字排至最後方 所以下一次可以少比較一個數
     return num #回傳list
 
